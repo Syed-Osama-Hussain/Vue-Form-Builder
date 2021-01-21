1ebdb67b4170e0547ac3354943045c9cba4dc6f2
Syed-Osama-Hussain <osamahussain38@gmail.com>
2021-01-21 20:32:20 +0500

    Form list in progress

---
+++ src/components/FormCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div @click="$emit('showDetail', form._id)">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "FormCard",
+  props: {
+    form:{
+        type: Object,
+        required: true
+    }
+  }
+};
+</script>
---
+++ src/components/FormList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <form-card v-for="form in forms" :key="form._id" :form="form"></form-card>
+    </div>
+  
+</template>
+
+<script>
+import { getForms } from "@/services/formService";
+import FormCard from "@/components/FormCard";
+
+export default {
+  name: "FormList",
+  async created() {
+    this.forms = await getForms();
+  },
+  data() {
+    return {
+      forms: []
+    };
+  },
+  methods: {
+    handleClick(id) {
+      this.$router.push({ name: "FormDetail", params:{ id } });
+    }
+  }
+};
+</script>
